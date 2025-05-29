4196196500986216a5463708ed83956775a882d0
Mayara Almeida <jennim.almeida@gmail.com>
2025-05-29 15:46:06 -0300

    Tela de Login - HTML e CSS

---
+++ constants/colors.js
@@ -0,0 +1,20 @@
+export const colors = {
+navy: '#071739',
+steel: '#4B6382',
+gray: '#A4B5C4',
+gainsboro: '#CDD5DB',
+khaki: '#A68866',
+sand: '#E3C39D'
+}
+
+// #071739 – Midnight Navy (Azul-marinho escuro, quase preto)
+
+// #4B6382 – Steel Blue (Azul aço suave)
+
+// #A4B5C4 – Light Slate Gray (Cinza azulado claro)
+
+// #CDD5DB – Gainsboro (Cinza muito claro com toque azulado)
+
+// #A68866 – Khaki Brown (Marrom cáqui suave)
+
+// #E3C39D – Desert Sand (Areia do deserto, bege quente)
